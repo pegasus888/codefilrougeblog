b653af4c4463828b1f349afde0aeaed92a145dff
pegasus888 <85719175+pegasus888@users.noreply.github.com>
2024-06-09 21:27:57 +0200

    Users page

--- app/pages/admin/users.php
+++ app/pages/admin/users.php
@@ -1,3 +1,9 @@
-<h1>Users</h1>
+<h1>Users
+    <button class="btn btn-primary">Add new</button>
+</h1>
 
-
+<table class="table">
+    <tr>
+        <th>#</th><th>Username</th><th>Email</th><th>Role</th><th>Image</th><th>Date</th>
+    </tr>
+</table>
