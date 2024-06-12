81ac7187fb99e030fd35d1497c67e4ddd2b0f623
pegasus888 <85719175+pegasus888@users.noreply.github.com>
2024-06-12 07:48:31 +0200

    Table responsive in Users page

--- app/pages/admin/users.php
+++ app/pages/admin/users.php
@@ -1,9 +1,32 @@
 <h1>Users
-    <button class="btn btn-primary">Add new</button>
+    <a href="" class="">
+    <button class="btn btn-primary text-info">Add new</button>
+    </a>
 </h1>
 
+<div class="table-responsive">
 <table class="table">
+
     <tr>
-        <th>#</th><th>Username</th><th>Email</th><th>Role</th><th>Image</th><th>Date</th>
+        <th>#</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Image</th>
+        <th>Date</th>
     </tr>
+
+    <tr>
+        <td>#</td>
+        <td>Username</td>
+        <td>Email</td>
+        <td>Role</td>
+        <td>Image</td>
+        <td>Date</td>
+    </tr>
+    <button class="btn btn-warning btn-sm text-info"><i class="bi bi-vector-pen"></i></button>
+    <button class="btn btn-danger btn-sm text-white"><i class="bi bi-trash3"></i></button>
+
+
 </table>
+</div>
